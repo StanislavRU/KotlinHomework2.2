f9ffe4f4a6d267b4b7111bff6e3319580b846e8d
Stanislav Kominov <stanislav.com@inbox.ru>
2021-08-09 13:39:01 +0500

    corrections of comments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,9 @@
 fun main() {
-    val likes = 10221
-    var i = 1
-    var j = 11
-    var value = true
+    val likes = 15121
 
-    while (likes >= i) {
-        if (i === likes) {
-            value = false
-        }
-        if (j === likes) {
-            value = true
-            break
-        }
-        i += 10
-        j += 100
-    }
-    if (value) {
-        println("Понравилось $likes людям")
-    } else {
+    if (likes%10 == 1 && likes%100 != 11) {
         println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
     }
 }
